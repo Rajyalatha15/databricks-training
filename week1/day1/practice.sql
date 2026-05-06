42d6aa06e1b84e8368ceee0d85303944484632d8
Rajyalatha15 <rajyalathajogi2006@gmail.com>
2026-05-06 21:44:43 +0530

    Added SQL queries for Day 1

--- week1/day1/practice.sql
+++ week1/day1/practice.sql
@@ -1,1 +1,27 @@
+-- =========================
+-- DAY 1 SQL PRACTICE
+-- =========================
 
+-- Q1
+SELECT *
+FROM Employee;
+
+-- Q2
+SELECT name, salary
+FROM Employee;
+
+-- Q3
+SELECT *
+FROM Employee
+WHERE age > 30;
+
+-- Q4
+SELECT name
+FROM Department;
+
+-- Q5
+SELECT e.* 
+FROM Employee e
+JOIN Department d 
+ON e.department_id = d.department_id
+WHERE d.name = 'IT';
